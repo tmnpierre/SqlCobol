ff205a438b1e81f706dd7a85d0f49954b4e9ff12
tmnpierre <tomyn.pierre@gmail.com>
2024-05-17 14:09:42 +0200

    initial commit

--- Partie3/GenRpt.cbl
+++ Partie3/GenRpt.cbl
@@ -12,11 +12,17 @@
        01  MAX-AGE              PIC 99 VALUE ZEROS.
        01  MEDIAN-AGE           PIC 99 VALUE ZEROS.
        01  COUNTRY              PIC X(50) VALUE SPACES.
-       01  MALE-PROP            PIC 9999 VALUE ZEROS.
+       01  CURRENT-COUNTRY      PIC X(50) VALUE SPACES.
+       01  GENDER               PIC X(10) VALUE SPACES.
+       01  MALE-COUNT           PIC 9999 VALUE ZEROS.
+       01  FEMALE-COUNT         PIC 9999 VALUE ZEROS.
+       01  OTHER-COUNT          PIC 9999 VALUE ZEROS.
+       01  TOTAL-COUNT          PIC 9999 VALUE ZEROS.
+       01  MALE-PROP            PIC 999 VALUE ZEROS.
        01  MALE-PROP-DISP       PIC Z99,99.
-       01  FEMALE-PROP          PIC 9999 VALUE ZEROS.
+       01  FEMALE-PROP          PIC 999 VALUE ZEROS.
        01  FEMALE-PROP-DISP     PIC Z99,99.
-       01  OTHER-PROP           PIC 9999 VALUE ZEROS.
+       01  OTHER-PROP           PIC 999 VALUE ZEROS.
        01  OTHER-PROP-DISP      PIC Z99,99.
        01  REPORT-LINE          PIC X(80) VALUE SPACES.
        01  DASH-LINE            PIC X(80) VALUE ALL '-'.
@@ -40,9 +46,6 @@
                PERFORM 1001-ERROR-RTN-START
                    THRU 1001-ERROR-RTN-END
            END-IF.
-
-           PERFORM 3000-SETUP-GENDER-PROPORTIONS-TABLE
-               THRU 3000-SETUP-GENDER-PROPORTIONS-TABLE-END.
 
            PERFORM 2000-GENERATE-REPORT
                THRU 2000-GENERATE-REPORT-END.
@@ -90,24 +93,38 @@
       ******************************************************************
        2100-GET-AGE-STATISTICS.
            EXEC SQL
-               SELECT MIN(age), MAX(age), PERCENTILE_CONT(0.5) 
-                   WITHIN GROUP (ORDER BY age)
-               INTO :MIN-AGE, :MAX-AGE, :MEDIAN-AGE
+               SELECT MIN(age), MAX(age)
+               INTO :MIN-AGE, :MAX-AGE
                FROM databank
            END-EXEC.
 
            DISPLAY 'Age Statistics: '.
            DISPLAY 'Minimum Age: ' MIN-AGE.
            DISPLAY 'Maximum Age: ' MAX-AGE.
+           DISPLAY DASH-LINE.
+
+           EXEC SQL
+               SELECT age
+               INTO :MEDIAN-AGE
+               FROM (
+                   SELECT age
+                   FROM databank
+                   ORDER BY age
+                   FETCH FIRST 50 PERCENT ROWS ONLY
+               ) AS subquery
+               ORDER BY age DESC
+               FETCH FIRST 1 ROW ONLY
+           END-EXEC.
+
            DISPLAY 'Median Age: ' MEDIAN-AGE.
            DISPLAY DASH-LINE.
        2100-GET-AGE-STATISTICS-END.
       ******************************************************************
        2200-GET-GENDER-PROPORTIONS.
            EXEC SQL DECLARE COUNTRY_CUR CURSOR FOR
-               SELECT country, male_proportion, female_proportion,
-                    other_proportion
-               FROM gender_proportions
+               SELECT country, gender
+               FROM databank
+               ORDER BY country
            END-EXEC.
 
            EXEC SQL OPEN COUNTRY_CUR END-EXEC.
@@ -115,23 +132,64 @@
            PERFORM WITH TEST AFTER UNTIL SQLCODE = +100
                EXEC SQL
                    FETCH COUNTRY_CUR
-                   INTO :COUNTRY, :MALE-PROP, :FEMALE-PROP, :OTHER-PROP
+                   INTO :COUNTRY, :GENDER
                END-EXEC
 
                IF SQLCODE = 0 THEN
-                   DISPLAY 'Country: ' COUNTRY
-                   DISPLAY DASH-LINE
-                   MOVE MALE-PROP TO MALE-PROP-DISP
-                   MOVE FEMALE-PROP TO FEMALE-PROP-DISP
-                   MOVE OTHER-PROP TO OTHER-PROP-DISP
-                   PERFORM 2210-DISPLAY-GENDER-PROPORTIONS
-                       THRU 2210-DISPLAY-GENDER-PROPORTIONS-END
-                   DISPLAY DASH-LINE
+                   IF COUNTRY NOT = CURRENT-COUNTRY THEN
+                       IF CURRENT-COUNTRY NOT = SPACES THEN
+                           PERFORM 
+                         2300-CALCULATE-AND-DISPLAY-GENDER-PROPORTIONS
+                               THRU 
+                      2300-CALCULATE-AND-DISPLAY-GENDER-PROPORTIONS-END
+                       END-IF
+                       MOVE COUNTRY TO CURRENT-COUNTRY
+                       MOVE ZEROS TO MALE-COUNT, FEMALE-COUNT, 
+                                     OTHER-COUNT, TOTAL-COUNT
+                   END-IF
+
+                   ADD 1 TO TOTAL-COUNT
+
+                   EVALUATE GENDER
+                       WHEN 'Male'
+                           ADD 1 TO MALE-COUNT
+                       WHEN 'Female'
+                           ADD 1 TO FEMALE-COUNT
+                       WHEN OTHER
+                           ADD 1 TO OTHER-COUNT
+                   END-EVALUATE
                END-IF
            END-PERFORM.
 
+           PERFORM 2300-CALCULATE-AND-DISPLAY-GENDER-PROPORTIONS
+               THRU 2300-CALCULATE-AND-DISPLAY-GENDER-PROPORTIONS-END
+
            EXEC SQL CLOSE COUNTRY_CUR END-EXEC.
        2200-GET-GENDER-PROPORTIONS-END.
+      ******************************************************************
+       2300-CALCULATE-AND-DISPLAY-GENDER-PROPORTIONS.
+           IF TOTAL-COUNT NOT = 0 THEN
+               COMPUTE MALE-PROP = (MALE-COUNT * 100) / TOTAL-COUNT
+               COMPUTE FEMALE-PROP = (FEMALE-COUNT * 100) / TOTAL-COUNT
+               COMPUTE OTHER-PROP = (OTHER-COUNT * 100) / TOTAL-COUNT
+           ELSE
+               MOVE ZEROS TO MALE-PROP
+               MOVE ZEROS TO FEMALE-PROP
+               MOVE ZEROS TO OTHER-PROP
+           END-IF
+
+           MOVE MALE-PROP TO MALE-PROP-DISP
+           MOVE FEMALE-PROP TO FEMALE-PROP-DISP
+           MOVE OTHER-PROP TO OTHER-PROP-DISP
+
+           DISPLAY 'Country: ' CURRENT-COUNTRY
+           DISPLAY DASH-LINE
+
+           PERFORM 2210-DISPLAY-GENDER-PROPORTIONS
+               THRU 2210-DISPLAY-GENDER-PROPORTIONS-END
+
+           DISPLAY DASH-LINE.
+       2300-CALCULATE-AND-DISPLAY-GENDER-PROPORTIONS-END.
       ******************************************************************
        2210-DISPLAY-GENDER-PROPORTIONS.
            STRING 'Gender: Male, Proportion: ', MALE-PROP-DISP, ' %'
@@ -149,50 +207,4 @@
                   INTO REPORT-LINE
            DISPLAY REPORT-LINE.
        2210-DISPLAY-GENDER-PROPORTIONS-END.
-      ******************************************************************
-       3000-SETUP-GENDER-PROPORTIONS-TABLE.
-           EXEC SQL
-               DROP TABLE IF EXISTS gender_proportions
-           END-EXEC.
-
-           EXEC SQL
-               CREATE TABLE gender_proportions (
-                   country VARCHAR(50) PRIMARY KEY,
-                   male_proportion NUMERIC(5, 2),
-                   female_proportion NUMERIC(5, 2),
-                   other_proportion NUMERIC(5, 2)
-               )
-           END-EXEC.
-
-           EXEC SQL
-               WITH gender_counts AS (
-                   SELECT
-                       country,
-                       COUNT(*) FILTER (WHERE gender = 'Male') 
-                           AS male_count,
-                       COUNT(*) FILTER (WHERE gender = 'Female') 
-                           AS female_count,
-                       COUNT(*) FILTER 
-                           (WHERE gender NOT IN ('Male', 'Female')) 
-                           AS other_count,
-                       COUNT(*) AS total_count
-                   FROM
-                       databank
-                   GROUP BY
-                       country
-               )
-               INSERT INTO gender_proportions 
-                   (country, male_proportion, female_proportion, 
-                    other_proportion)
-               SELECT
-                   country,
-                   ROUND(male_count * 100.0 / 
-                       NULLIF(total_count, 0), 2),
-                   ROUND(female_count * 100.0 / 
-                       NULLIF(total_count, 0), 2),
-                   ROUND(other_count * 100.0 / 
-                       NULLIF(total_count, 0), 2)
-               FROM gender_counts
-           END-EXEC.
-       3000-SETUP-GENDER-PROPORTIONS-TABLE-END.
       ******************************************************************
--- Partie3/GenRpt.cob
+++ Partie3/GenRpt.cob
@@ -12,11 +12,17 @@
        01  MAX-AGE              PIC 99 VALUE ZEROS.
        01  MEDIAN-AGE           PIC 99 VALUE ZEROS.
        01  COUNTRY              PIC X(50) VALUE SPACES.
-       01  MALE-PROP            PIC 9999 VALUE ZEROS.
+       01  CURRENT-COUNTRY      PIC X(50) VALUE SPACES.
+       01  GENDER               PIC X(10) VALUE SPACES.
+       01  MALE-COUNT           PIC 9999 VALUE ZEROS.
+       01  FEMALE-COUNT         PIC 9999 VALUE ZEROS.
+       01  OTHER-COUNT          PIC 9999 VALUE ZEROS.
+       01  TOTAL-COUNT          PIC 9999 VALUE ZEROS.
+       01  MALE-PROP            PIC 999 VALUE ZEROS.
        01  MALE-PROP-DISP       PIC Z99,99.
-       01  FEMALE-PROP          PIC 9999 VALUE ZEROS.
+       01  FEMALE-PROP          PIC 999 VALUE ZEROS.
        01  FEMALE-PROP-DISP     PIC Z99,99.
-       01  OTHER-PROP           PIC 9999 VALUE ZEROS.
+       01  OTHER-PROP           PIC 999 VALUE ZEROS.
        01  OTHER-PROP-DISP      PIC Z99,99.
        01  REPORT-LINE          PIC X(80) VALUE SPACES.
        01  DASH-LINE            PIC X(80) VALUE ALL '-'.
@@ -37,42 +43,19 @@ OCESQL     02  FILLER PIC X(014) VALUE "DISCONNECT ALL".
 OCESQL     02  FILLER PIC X(1) VALUE X"00".
 OCESQL*
 OCESQL 01  SQ0002.
-OCESQL     02  FILLER PIC X(089) VALUE "SELECT MIN(age), MAX(age), PER"
-OCESQL  &  "CENTILE_CONT(0.5) WITHIN GROUP (ORDER BY age) FROM databan"
-OCESQL  &  "k".
+OCESQL     02  FILLER PIC X(039) VALUE "SELECT MIN(age), MAX(age) FROM"
+OCESQL  &  " databank".
 OCESQL     02  FILLER PIC X(1) VALUE X"00".
 OCESQL*
 OCESQL 01  SQ0003.
-OCESQL     02  FILLER PIC X(092) VALUE "SELECT country, male_proportio"
-OCESQL  &  "n, female_proportion, other_proportion FROM gender_proport"
-OCESQL  &  "ions".
+OCESQL     02  FILLER PIC X(143) VALUE "SELECT age FROM ( SELECT age F"
+OCESQL  &  "ROM databank ORDER BY age FETCH FIRST 50 PERCENT ROWS ONLY"
+OCESQL  &  " ) AS subquery ORDER BY age DESC FETCH FIRST 1 ROW ONLY".
 OCESQL     02  FILLER PIC X(1) VALUE X"00".
 OCESQL*
 OCESQL 01  SQ0004.
-OCESQL     02  FILLER PIC X(039) VALUE "DROP TABLE IF EXISTS gender_pr"
-OCESQL  &  "oportions".
-OCESQL     02  FILLER PIC X(1) VALUE X"00".
-OCESQL*
-OCESQL 01  SQ0005.
-OCESQL     02  FILLER PIC X(163) VALUE "CREATE TABLE gender_proportion"
-OCESQL  &  "s ( country VARCHAR(50) PRIMARY KEY, male_proportion NUMER"
-OCESQL  &  "IC(5, 2), female_proportion NUMERIC(5, 2), other_proportio"
-OCESQL  &  "n NUMERIC(5, 2) )".
-OCESQL     02  FILLER PIC X(1) VALUE X"00".
-OCESQL*
-OCESQL 01  SQ0007.
-OCESQL     02  FILLER PIC X(256) VALUE "WITH gender_counts AS ( SELECT"
-OCESQL  &  " country, COUNT( * ) FILTER (WHERE gender = 'Male') AS mal"
-OCESQL  &  "e_count, COUNT( * ) FILTER (WHERE gender = 'Female') AS fe"
-OCESQL  &  "male_count, COUNT( * ) FILTER (WHERE gender NOT IN ('Male'"
-OCESQL  &  ", 'Female')) AS other_count, COUNT( * ) AS total_cou".
-OCESQL     02  FILLER PIC X(256) VALUE "nt FROM databank GROUP BY coun"
-OCESQL  &  "try ) INSERT INTO gender_proportions (country, male_propor"
-OCESQL  &  "tion, female_proportion, other_proportion) SELECT country,"
-OCESQL  &  " ROUND(male_count * 100.0 / NULLIF(total_count, 0), 2), RO"
-OCESQL  &  "UND(female_count * 100.0 / NULLIF(total_count, 0), 2".
-OCESQL     02  FILLER PIC X(076) VALUE "), ROUND(other_count * 100.0 /"
-OCESQL  &  " NULLIF(total_count, 0), 2) FROM gender_counts".
+OCESQL     02  FILLER PIC X(053) VALUE "SELECT country, gender FROM da"
+OCESQL  &  "tabank ORDER BY country".
 OCESQL     02  FILLER PIC X(1) VALUE X"00".
 OCESQL*
        PROCEDURE DIVISION.
@@ -94,9 +77,6 @@ OCESQL     END-CALL.
                PERFORM 1001-ERROR-RTN-START
                    THRU 1001-ERROR-RTN-END
            END-IF.
-
-           PERFORM 3000-SETUP-GENDER-PROPORTIONS-TABLE
-               THRU 3000-SETUP-GENDER-PROPORTIONS-TABLE-END.
 
            PERFORM 2000-GENERATE-REPORT
                THRU 2000-GENERATE-REPORT-END.
@@ -163,9 +143,8 @@ OCESQL     END-CALL
       ******************************************************************
        2100-GET-AGE-STATISTICS.
 OCESQL*    EXEC SQL
-OCESQL*        SELECT MIN(age), MAX(age), PERCENTILE_CONT(0.5) 
-OCESQL*            WITHIN GROUP (ORDER BY age)
-OCESQL*        INTO :MIN-AGE, :MAX-AGE, :MEDIAN-AGE
+OCESQL*        SELECT MIN(age), MAX(age)
+OCESQL*        INTO :MIN-AGE, :MAX-AGE
 OCESQL*        FROM databank
 OCESQL*    END-EXEC.
 OCESQL     CALL "OCESQLStartSQL"
@@ -182,6 +161,34 @@ OCESQL          BY VALUE 2
 OCESQL          BY VALUE 0
 OCESQL          BY REFERENCE MAX-AGE
 OCESQL     END-CALL
+OCESQL     CALL "OCESQLExecSelectIntoOne" USING
+OCESQL          BY REFERENCE SQLCA
+OCESQL          BY REFERENCE SQ0002
+OCESQL          BY VALUE 0
+OCESQL          BY VALUE 2
+OCESQL     END-CALL
+OCESQL     CALL "OCESQLEndSQL"
+OCESQL     END-CALL.
+
+           DISPLAY 'Age Statistics: '.
+           DISPLAY 'Minimum Age: ' MIN-AGE.
+           DISPLAY 'Maximum Age: ' MAX-AGE.
+           DISPLAY DASH-LINE.
+
+OCESQL*    EXEC SQL
+OCESQL*        SELECT age
+OCESQL*        INTO :MEDIAN-AGE
+OCESQL*        FROM (
+OCESQL*            SELECT age
+OCESQL*            FROM databank
+OCESQL*            ORDER BY age
+OCESQL*            FETCH FIRST 50 PERCENT ROWS ONLY
+OCESQL*        ) AS subquery
+OCESQL*        ORDER BY age DESC
+OCESQL*        FETCH FIRST 1 ROW ONLY
+OCESQL*    END-EXEC.
+OCESQL     CALL "OCESQLStartSQL"
+OCESQL     END-CALL
 OCESQL     CALL "OCESQLSetResultParams" USING
 OCESQL          BY VALUE 1
 OCESQL          BY VALUE 2
@@ -190,30 +197,27 @@ OCESQL          BY REFERENCE MEDIAN-AGE
 OCESQL     END-CALL
 OCESQL     CALL "OCESQLExecSelectIntoOne" USING
 OCESQL          BY REFERENCE SQLCA
-OCESQL          BY REFERENCE SQ0002
+OCESQL          BY REFERENCE SQ0003
 OCESQL          BY VALUE 0
-OCESQL          BY VALUE 3
+OCESQL          BY VALUE 1
 OCESQL     END-CALL
 OCESQL     CALL "OCESQLEndSQL"
 OCESQL     END-CALL.
 
-           DISPLAY 'Age Statistics: '.
-           DISPLAY 'Minimum Age: ' MIN-AGE.
-           DISPLAY 'Maximum Age: ' MAX-AGE.
            DISPLAY 'Median Age: ' MEDIAN-AGE.
            DISPLAY DASH-LINE.
        2100-GET-AGE-STATISTICS-END.
       ******************************************************************
        2200-GET-GENDER-PROPORTIONS.
 OCESQL*    EXEC SQL DECLARE COUNTRY_CUR CURSOR FOR
-OCESQL*        SELECT country, male_proportion, female_proportion,
-OCESQL*             other_proportion
-OCESQL*        FROM gender_proportions
+OCESQL*        SELECT country, gender
+OCESQL*        FROM databank
+OCESQL*        ORDER BY country
 OCESQL*    END-EXEC.
 OCESQL     CALL "OCESQLCursorDeclare" USING
 OCESQL          BY REFERENCE SQLCA
 OCESQL          BY REFERENCE "GenRpt_COUNTRY_CUR" & x"00"
-OCESQL          BY REFERENCE SQ0003
+OCESQL          BY REFERENCE SQ0004
 OCESQL     END-CALL.
 
 OCESQL*    EXEC SQL OPEN COUNTRY_CUR END-EXEC.
@@ -225,7 +229,7 @@ OCESQL     END-CALL.
            PERFORM WITH TEST AFTER UNTIL SQLCODE = +100
 OCESQL*        EXEC SQL
 OCESQL*            FETCH COUNTRY_CUR
-OCESQL*            INTO :COUNTRY, :MALE-PROP, :FEMALE-PROP, :OTHER-PROP
+OCESQL*            INTO :COUNTRY, :GENDER
 OCESQL*        END-EXEC
 OCESQL     CALL "OCESQLStartSQL"
 OCESQL     END-CALL
@@ -236,22 +240,10 @@ OCESQL          BY VALUE 0
 OCESQL          BY REFERENCE COUNTRY
 OCESQL     END-CALL
 OCESQL     CALL "OCESQLSetResultParams" USING
-OCESQL          BY VALUE 1
-OCESQL          BY VALUE 4
+OCESQL          BY VALUE 16
+OCESQL          BY VALUE 10
 OCESQL          BY VALUE 0
-OCESQL          BY REFERENCE MALE-PROP
-OCESQL     END-CALL
-OCESQL     CALL "OCESQLSetResultParams" USING
-OCESQL          BY VALUE 1
-OCESQL          BY VALUE 4
-OCESQL          BY VALUE 0
-OCESQL          BY REFERENCE FEMALE-PROP
-OCESQL     END-CALL
-OCESQL     CALL "OCESQLSetResultParams" USING
-OCESQL          BY VALUE 1
-OCESQL          BY VALUE 4
-OCESQL          BY VALUE 0
-OCESQL          BY REFERENCE OTHER-PROP
+OCESQL          BY REFERENCE GENDER
 OCESQL     END-CALL
 OCESQL     CALL "OCESQLCursorFetchOne" USING
 OCESQL          BY REFERENCE SQLCA
@@ -261,16 +253,33 @@ OCESQL     CALL "OCESQLEndSQL"
 OCESQL     END-CALL
 
                IF SQLCODE = 0 THEN
-                   DISPLAY 'Country: ' COUNTRY
-                   DISPLAY DASH-LINE
-                   MOVE MALE-PROP TO MALE-PROP-DISP
-                   MOVE FEMALE-PROP TO FEMALE-PROP-DISP
-                   MOVE OTHER-PROP TO OTHER-PROP-DISP
-                   PERFORM 2210-DISPLAY-GENDER-PROPORTIONS
-                       THRU 2210-DISPLAY-GENDER-PROPORTIONS-END
-                   DISPLAY DASH-LINE
+                   IF COUNTRY NOT = CURRENT-COUNTRY THEN
+                       IF CURRENT-COUNTRY NOT = SPACES THEN
+                           PERFORM 
+                         2300-CALCULATE-AND-DISPLAY-GENDER-PROPORTIONS
+                               THRU 
+                      2300-CALCULATE-AND-DISPLAY-GENDER-PROPORTIONS-END
+                       END-IF
+                       MOVE COUNTRY TO CURRENT-COUNTRY
+                       MOVE ZEROS TO MALE-COUNT, FEMALE-COUNT, 
+                                     OTHER-COUNT, TOTAL-COUNT
+                   END-IF
+
+                   ADD 1 TO TOTAL-COUNT
+
+                   EVALUATE GENDER
+                       WHEN 'Male'
+                           ADD 1 TO MALE-COUNT
+                       WHEN 'Female'
+                           ADD 1 TO FEMALE-COUNT
+                       WHEN OTHER
+                           ADD 1 TO OTHER-COUNT
+                   END-EVALUATE
                END-IF
            END-PERFORM.
+
+           PERFORM 2300-CALCULATE-AND-DISPLAY-GENDER-PROPORTIONS
+               THRU 2300-CALCULATE-AND-DISPLAY-GENDER-PROPORTIONS-END
 
 OCESQL*    EXEC SQL CLOSE COUNTRY_CUR END-EXEC.
 OCESQL     CALL "OCESQLCursorClose"  USING
@@ -279,6 +288,30 @@ OCESQL          BY REFERENCE "GenRpt_COUNTRY_CUR" & x"00"
 OCESQL     END-CALL
 OCESQL    .
        2200-GET-GENDER-PROPORTIONS-END.
+      ******************************************************************
+       2300-CALCULATE-AND-DISPLAY-GENDER-PROPORTIONS.
+           IF TOTAL-COUNT NOT = 0 THEN
+               COMPUTE MALE-PROP = (MALE-COUNT * 100) / TOTAL-COUNT
+               COMPUTE FEMALE-PROP = (FEMALE-COUNT * 100) / TOTAL-COUNT
+               COMPUTE OTHER-PROP = (OTHER-COUNT * 100) / TOTAL-COUNT
+           ELSE
+               MOVE ZEROS TO MALE-PROP
+               MOVE ZEROS TO FEMALE-PROP
+               MOVE ZEROS TO OTHER-PROP
+           END-IF
+
+           MOVE MALE-PROP TO MALE-PROP-DISP
+           MOVE FEMALE-PROP TO FEMALE-PROP-DISP
+           MOVE OTHER-PROP TO OTHER-PROP-DISP
+
+           DISPLAY 'Country: ' CURRENT-COUNTRY
+           DISPLAY DASH-LINE
+
+           PERFORM 2210-DISPLAY-GENDER-PROPORTIONS
+               THRU 2210-DISPLAY-GENDER-PROPORTIONS-END
+
+           DISPLAY DASH-LINE.
+       2300-CALCULATE-AND-DISPLAY-GENDER-PROPORTIONS-END.
       ******************************************************************
        2210-DISPLAY-GENDER-PROPORTIONS.
            STRING 'Gender: Male, Proportion: ', MALE-PROP-DISP, ' %'
@@ -296,64 +329,6 @@ OCESQL    .
                   INTO REPORT-LINE
            DISPLAY REPORT-LINE.
        2210-DISPLAY-GENDER-PROPORTIONS-END.
-      ******************************************************************
-       3000-SETUP-GENDER-PROPORTIONS-TABLE.
-OCESQL*    EXEC SQL
-OCESQL*        DROP TABLE IF EXISTS gender_proportions
-OCESQL*    END-EXEC.
-OCESQL     CALL "OCESQLExec" USING
-OCESQL          BY REFERENCE SQLCA
-OCESQL          BY REFERENCE SQ0004
-OCESQL     END-CALL.
-
-OCESQL*    EXEC SQL
-OCESQL*        CREATE TABLE gender_proportions (
-OCESQL*            country VARCHAR(50) PRIMARY KEY,
-OCESQL*            male_proportion NUMERIC(5, 2),
-OCESQL*            female_proportion NUMERIC(5, 2),
-OCESQL*            other_proportion NUMERIC(5, 2)
-OCESQL*        )
-OCESQL*    END-EXEC.
-OCESQL     CALL "OCESQLExec" USING
-OCESQL          BY REFERENCE SQLCA
-OCESQL          BY REFERENCE SQ0005
-OCESQL     END-CALL.
-
-OCESQL*    EXEC SQL
-OCESQL*        WITH gender_counts AS (
-OCESQL*            SELECT
-OCESQL*                country,
-OCESQL*                COUNT(*) FILTER (WHERE gender = 'Male') 
-OCESQL*                    AS male_count,
-OCESQL*                COUNT(*) FILTER (WHERE gender = 'Female') 
-OCESQL*                    AS female_count,
-OCESQL*                COUNT(*) FILTER 
-OCESQL*                    (WHERE gender NOT IN ('Male', 'Female')) 
-OCESQL*                    AS other_count,
-OCESQL*                COUNT(*) AS total_count
-OCESQL*            FROM
-OCESQL*                databank
-OCESQL*            GROUP BY
-OCESQL*                country
-OCESQL*        )
-OCESQL*        INSERT INTO gender_proportions 
-OCESQL*            (country, male_proportion, female_proportion, 
-OCESQL*             other_proportion)
-OCESQL*        SELECT
-OCESQL*            country,
-OCESQL*            ROUND(male_count * 100.0 / 
-OCESQL*                NULLIF(total_count, 0), 2),
-OCESQL*            ROUND(female_count * 100.0 / 
-OCESQL*                NULLIF(total_count, 0), 2),
-OCESQL*            ROUND(other_count * 100.0 / 
-OCESQL*                NULLIF(total_count, 0), 2)
-OCESQL*        FROM gender_counts
-OCESQL*    END-EXEC.
-OCESQL     CALL "OCESQLExec" USING
-OCESQL          BY REFERENCE SQLCA
-OCESQL          BY REFERENCE SQ0007
-OCESQL     END-CALL.
-       3000-SETUP-GENDER-PROPORTIONS-TABLE-END.
       ******************************************************************
       ******************************************************************
       ******************************************************************
